9101d2721b2d310a876f27eec4a07657bfc74b09
Alexios Lampouridis <alexlampouridis@gmail.com>
2021-07-06 02:50:44 +0300

    -Fix import

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getPersons } from './actions/personAction'
+import CreateDelay from './createDelay/CreateDelay'
 
 class Home extends React.Component {
 
